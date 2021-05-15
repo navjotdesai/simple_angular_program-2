8ce36c51b157687fe5406c80c974e594fe49239e
Navjot <navjotdesai56@gmail.com>
2021-05-15 15:56:19 +0530

    Initial Commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,17 @@
+import {Hero} from './hero'
+
+export const HEROES : Hero[] = [
+   
+  { id: 11, name: 'Dr Strange'},
+  { id: 12, name: 'Batman' },
+  { id: 13, name: 'Iron Man' },
+  { id: 14, name: 'Spiderman' },
+  { id: 15, name: 'Captian America' },
+  { id: 16, name: 'Superman' },
+  { id: 17, name: 'Ant Man' },
+  { id: 18, name: 'Hulk' },
+  { id: 19, name: 'Thor' },
+  { id: 20, name: 'Tornado' }
+
+];
+
